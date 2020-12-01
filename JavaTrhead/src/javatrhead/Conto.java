8b0a3994086b75b4ab3762663b8740bd47aef146
meregalli.amos <meregalli.amos@TAVE-PC21.OSDBSESTO.local>
2020-12-01 13:42:19 +0100

    bello esercizio

---
+++ JavaTrhead/src/javatrhead/Conto.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javatrhead;
+
+/**
+ *
+ * @author meregalli.amos
+ */
+public class Conto {
+    private int totale;
+    private final int tasse=100;
+    
+    public Conto(int totale) {
+        this.totale = totale;
+    }
+    synchronized void versa(int somma){
+        totale=totale+somma-tasse;
+        System.out.println("Conto: "+this.saldo());
+    }
+    synchronized int saldo(){
+        return totale;
+    }
+}
